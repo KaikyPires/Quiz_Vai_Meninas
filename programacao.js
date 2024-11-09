97c5b367bc75374d70f2de803d40e1c77dcd034b
Kaiky Pires <kaiky.pires@yahoo.com.br>
2024-11-08 21:32:25 -0300

    commit

---
+++ programacao.js
@@ -0,0 +1,59 @@
+export default [
+    {
+      question: "Qual é a linguagem de programação amplamente usada para o desenvolvimento web front-end?",
+      answers: [
+        { option: "JavaScript", correct: true },
+        { option: "Python", correct: false },
+        { option: "C#", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o propósito do comando 'for' em muitas linguagens de programação?",
+      answers: [
+        { option: "Iterar sobre uma sequência de valores", correct: true },
+        { option: "Definir uma função", correct: false },
+        { option: "Declarar uma variável", correct: false },
+      ],
+    },
+    {
+      question: "Qual dos seguintes conceitos está associado à orientação a objetos?",
+      answers: [
+        { option: "Encapsulamento", correct: true },
+        { option: "Funções anônimas", correct: false },
+        { option: "Procedimentos", correct: false },
+      ],
+    },
+    {
+      question: "Qual é a estrutura de dados usada para armazenar pares chave-valor em Python?",
+      answers: [
+        { option: "Dicionário", correct: true },
+        { option: "Lista", correct: false },
+        { option: "Conjunto", correct: false },
+      ],
+    },
+    {
+      question: "Qual comando em SQL é utilizado para atualizar dados em uma tabela?",
+      answers: [
+        { option: "UPDATE", correct: true },
+        { option: "ALTER", correct: false },
+        { option: "INSERT", correct: false },
+      ],
+    },
+    {
+      question: "Em qual linguagem de programação a palavra-chave 'class' é usada para definir uma classe?",
+      answers: [
+        { option: "Java", correct: true },
+        { option: "HTML", correct: false },
+        { option: "SQL", correct: false },
+      ],
+    },
+    {
+      question: "Qual é o nome do processo de encontrar e corrigir erros em um programa?",
+      answers: [
+        { option: "Depuração", correct: true },
+        { option: "Compilação", correct: false },
+        { option: "Execução", correct: false },
+      ],
+    },
+  ];
+  
