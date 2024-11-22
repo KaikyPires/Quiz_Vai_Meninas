ffee1b71aa30f381ad7cc4cb89d20f3cc686c022
Kaiky Pires <kaiky.pires@yahoo.com.br>
2024-11-22 20:53:47 -0300

    commit

--- programacao.js
+++ programacao.js
@@ -1,61 +1,83 @@
 const questions = [
   {
-    question: "O que significa 'HTML'?",
+    question: "O que é Tecnologia?",
     answers: [
-    
-      { option: "Um programa de desenho", correct: false },
-      { option: "Um jogo online", correct: false },
-      { option: "Uma linguagem para criar páginas de internet", correct: true },
-      { option: "Um tipo de celular", correct: false }
-    ]
-  },
- 
-  {
-    question: "Qual é a função da tag <a> no HTML?",
-    answers: [
-
-      { option: "Fazer uma lista", correct: false },
-      { option: "Colocar um link", correct: true },
-      { option: "Inserir uma imagem", correct: false },
-      { option: "Colocar um parágrafo", correct: false }
+      { option: "Uma coisa que não usamos no dia a dia.", correct: false },
+      { option: "Coisas feitas por pessoas que usam conhecimento científico para resolver problemas.", correct: true },
+      { option: "Apenas computadores e celulares.", correct: false },
+      { option: "Uma forma de arte.", correct: false }
     ]
   },
   {
-    question: "Qual destes é um exemplo de navegador de internet?",
+    question: "O que é a Internet?",
     answers: [
-      { option: "Chrome", correct: true },
-      { option: "Word", correct: false },
-      { option: "Excel", correct: false },
-      { option: "Paint", correct: false }
+      { option: "Um lugar físico onde guardamos coisas.", correct: false },
+      { option: "Um jogo para computadores.", correct: false },
+      { option: "Uma rede que conecta computadores no mundo todo.", correct: true },
+      { option: "Apenas um programa usado para estudar.", correct: false }
     ]
   },
   {
-    question: "Qual é a função de um navegador de internet?",
+    question: "Qual o objetivo da Internet?",
     answers: [
-      { option: "Desenhar imagens", correct: false },
-      { option: "Ajudar a acessar sites na internet", correct: true },
-      { option: "Fazer cálculos matemáticos", correct: false },
-
-      { option: "Escrever textos no computador", correct: false }
+      { option: "Conectar pessoas e compartilhar informações.", correct: true },
+      { option: "Apenas assistir vídeos.", correct: false },
+      { option: "Ajudar as pessoas a fazerem cálculos.", correct: false },
+      { option: "Criar senhas complicadas.", correct: false }
     ]
   },
   {
-    question: "Qual é a primeira página que vemos quando entramos em um site?",
+    question: "Onde os sites ficam hospedados?",
     answers: [
-    
-      { option: "Página de contato", correct: false },
-      { option: "Página de produtos", correct: false },
-      { option: "Página de ajuda", correct: false },
-      { option: "Página inicial", correct: true }
+      { option: "Em computadores especiais chamados servidores.", correct: true },
+      { option: "No computador de quem criou o site.", correct: false },
+      { option: "Na internet, mas sem precisar de nenhum lugar físico.", correct: false },
+      { option: "No celular de quem acessa.", correct: false }
     ]
   },
   {
-    question: "Qual desses pode ser considerado um site?",
+    question: "Marque a opção que contém áreas profissionais da tecnologia.",
     answers: [
-      { option: "www.exemplo.com.br", correct: true },
-      { option: "Um computador", correct: false },
-      { option: "Um teclado", correct: false },
-      { option: "Uma imagem", correct: false }
+      { option: "Medicina, Engenharia Civil, Moda.", correct: false },
+      { option: "Programação, Segurança da Informação, Design de Sites.", correct: true },
+      { option: "Direito, Administração, Arquitetura.", correct: false },
+      { option: "Pintura, Música, Fotografia.", correct: false }
+    ]
+  },
+  {
+    question: "Quem foi Ada Lovelace?",
+    answers: [
+      { option: "Uma criadora de aplicativos.", correct: false },
+      { option: "Uma cientista que inventou a internet.", correct: false },
+      { option: "A primeira programadora da história.", correct: true },
+      { option: "Uma designer famosa.", correct: false }
+    ]
+  },
+  {
+    question: "O que é um Web Site?",
+    answers: [
+      { option: "Um programa para desenhar.", correct: false },
+      { option: "Uma página na internet com informações ou serviços.", correct: true },
+      { option: "Uma ferramenta para criar apresentações.", correct: false },
+      { option: "Apenas jogos na internet.", correct: false }
+    ]
+  },
+  {
+    question: "Qual será o tipo de site que vamos desenvolver nessa oficina?",
+    answers: [
+      { option: "Um site pessoal para compartilhar informações sobre você.", correct: true },
+      { option: "Um site de vendas de carros.", correct: false },
+      { option: "Um site para criar jogos.", correct: false },
+      { option: "Um site para assistir filmes.", correct: false }
+    ]
+  },
+  {
+    question: "O que iremos utilizar para criar o site pessoal nessa oficina?",
+    answers: [
+      { option: "Papel e caneta.", correct: false },
+      { option: "HTML e CSS.", correct: true },
+      { option: "Apenas o navegador.", correct: false },
+      { option: "Um aplicativo para redes sociais.", correct: false }
     ]
   }
 ];
